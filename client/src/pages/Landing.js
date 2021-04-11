73113e88390f8b847da5dae4ddd9794f12ebf602
Jon <wintersfile@gmail.com>
2021-04-11 16:03:04 -0500

    web3Modal toggleModal test

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -79,6 +79,7 @@ class Landing extends React.Component{
     async onConnect (){
         const provider = await this.web3Modal.connect();
         await this.subscribeProvider(provider);
+        await this.web3Modal.toggleModal();
         const web3 = initWeb3(provider);
         const accounts = await web3.eth.getAccounts();
         const address = accounts[0];
